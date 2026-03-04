969659f48007990ae6f69e5c38230f0eb9d97287
realzhangshen <dmlu62@gmail.com>
2026-03-04 11:28:43 +0000

    croot 0.2.1

--- Formula/croot.rb
+++ Formula/croot.rb
@@ -1,16 +1,16 @@
 class Croot < Formula
   desc "Lightweight terminal file tree sidebar with vim-style navigation"
   homepage "https://github.com/realzhangshen/croot"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/realzhangshen/croot/releases/download/v0.2.0/croot-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "9537ead11e6546a19160337d5933d1a65da4b4d651961896b02d387dd5fe0b2e"
+      url "https://github.com/realzhangshen/croot/releases/download/v0.2.1/croot-v0.2.1-aarch64-apple-darwin.tar.gz"
+      sha256 "30aa3b0f983d555f1783d78506c5fcc197ab8432e33f05804d6f17aef23a4067"
     else
-      url "https://github.com/realzhangshen/croot/releases/download/v0.2.0/croot-v0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "3d509f758470324e5d4d9713ba6e879162eaaef427b9a46e95508bdeb2d2f48a"
+      url "https://github.com/realzhangshen/croot/releases/download/v0.2.1/croot-v0.2.1-x86_64-apple-darwin.tar.gz"
+      sha256 "677f272e23200a42a485d6f438a73df663eabe54c3eedbd7e4a5ebd85f3004d4"
     end
   end
 
